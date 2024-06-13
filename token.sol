34b6c0af9036f15d6d00824f29b4a2b1053da5b2
Akrit <akritrihal57@gmail.com>
2024-06-13 11:53:25 +0530

    code file for token contract

---
+++ token.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract MyToken {
+    // Public variables here
+    string public tokenName = 'AkritToken';
+    string public tokenAbbrv = 'ATK';
+    uint public totalSupply;
+
+    // Mapping variable here
+    mapping(address => uint) public balances;
+
+    // Mint function
+    function mint(address _to, uint _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
+    }
+
+    // Burn function
+    function burn(address _from, uint _value) public {
+        if (balances[_from] >= _value) {
+            totalSupply -= _value;
+            balances[_from] -= _value;
+        }
+    }
+}
